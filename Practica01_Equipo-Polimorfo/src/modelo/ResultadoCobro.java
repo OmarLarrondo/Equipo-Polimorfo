4fcd98bd35f45a84dd3512f137125f8318f50c42
Soto Mendoza Ismael de Jesus <isma132006@ciencias.unam.mx>
2025-09-02 09:45:43 -0600

    Resultado cobro,GestorArchivos y cheque Simulacion

--- Practica01_Equipo-Polimorfo/src/modelo/ResultadoCobro.java
+++ Practica01_Equipo-Polimorfo/src/modelo/ResultadoCobro.java
@@ -1,5 +1,133 @@
 package modelo;
 
+
+import servicios.Servicio;
+
+
 public class ResultadoCobro {
     
+    /**EL usuario al que se cobrara */
+    private Usuario usuario;
+
+    /**EL servicio que realizara el cobro */
+    private String servicio;
+
+    /**EL monto que se cobrara al usuario */
+    private double monto;
+
+    /**Resultado del cobro*/
+    private boolean exitoso;
+
+    /**La descripcion del cobro realizado */
+    private String descripcion;
+
+    public ResultadoCobro(Usuario usuario, String servicio
+    , double monto, boolean exitoso, String descripcion) {
+        this.usuario = usuario;
+        this.servicio = servicio;
+        this.monto = monto;
+        this.exitoso = exitoso;
+        this.descripcion = descripcion;
+    }
+
+
+    //metodos getters
+
+
+    /**
+     * Obtiene el usuario al que se le realizó el cobro.
+     *
+     * @return la instancia de {@link Usuario} correspondiente al cobro
+     */ 
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    /**
+     * Obtiene el servicio que realizo el cobro
+     * 
+     * @return la instancia de {@link Servicio} correspondiente al cobro
+     */
+    public String getServicio() {
+        return servicio;
+    }
+
+    /**
+     * Obtiene el monto que se realizo
+     * 
+     * @return el monto cobrado
+     */
+
+    public double getMonto() {
+        return monto;
+    }
+
+    /**
+     * Indica si el cobro se realizo
+     * 
+     * @return {@code true} si el cobro se realizo correctamente 
+     *          {@code false} en caso contrario.
+     */
+    public boolean getExitoso() {
+        return exitoso;
+    }
+
+    /**
+     * Obtiene la descripcion a detalle dle cobro
+     * 
+     * @return la descripcion del resultado del cobro
+     */
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+
+    //metodos setters
+
+
+    /**
+     * Establece el usuario al que se le realizó el cobro.
+     *
+     * @param usuario la instancia de {@link Usuario} asociada al cobro
+     */
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    /**
+     * Establece el nombre del servicio por el cual se realiza el cobro.
+     *
+     * @param servicio el nombre del servicio
+     */
+    public void setServicio(String servicio) {
+        this.servicio = servicio;
+    }
+
+    /**
+     * Establece el monto del cobro.
+     *
+     * @param monto la cantidad a cobrar
+     */
+    public void setMonto(double monto) {
+        this.monto = monto;
+    }
+
+    /**
+     * Establece si el cobro fue exitoso o fallido.
+     *
+     * @param exitoso true si el cobro fue exitoso, false en caso contrario
+     */
+    public void setExitoso(boolean exitoso) {
+        this.exitoso = exitoso;
+    }
+
+    /**
+     * Establece la descripción o detalle del resultado del cobro.
+     *
+     * @param descripcion el texto descriptivo del resultado del cobro
+     */
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }    
 }
--- Practica01_Equipo-Polimorfo/src/simulacion/Simulacion.java
+++ Practica01_Equipo-Polimorfo/src/simulacion/Simulacion.java
@@ -1,5 +1,230 @@
 package simulacion;
 
+import java.util.List;
+
+import estrategias.MomazonPremium;
+
+import java.util.ArrayList;
+
+
+import modelo.Usuario;
+import patrones.EstrategiaCobro;
+import servicios.HVOMax;
+import servicios.MomazonPrime;
+import servicios.Servicio;
+import servicios.Spootify;
+import servicios.Memeflix;
+import utilidades.GestorArchivos;
+
+/**
+ * Clase para orquestar toda la interacción temporal del sistema, 
+ * ejecutando los comportamientos específicos de cada usuario en cada mes 
+ * según las especificaciones de la práctica
+ * 
+ * Administra la lista de usuarios, los servicios disponibles, el registro de eventos 
+ * de la simulación y la escritura de reportes en archivos.
+ * 
+ * Esta clase modela un escenario en el que distintos usuarios aplican 
+ * comportamientos (como Alicia, Bob, etc.) a lo largo de los meses simulados.
+ * 
+ * @author Equipo-Polimorfo  
+ */
 public class Simulacion {
-    
+
+    /**Lista de usuarios registrados en la simulación.*/
+    private List<Usuario> usuarios;
+
+    /**Lista de servicios disponibles en la simulación.*/
+    private List<Servicio> servicios;
+
+    /** Número que representa el mes actual dentro de la simulación. [1-12]*/
+    private int mesActual;
+
+    /** Registro completo de todas las operaciones realizadas en la simulación.*/
+    private List<String> registroCompleto;
+
+    /** Gestor para todas las transacciones hechas en cada mes y para cada cliente.*/
+    private GestorArchivos gestorArchivos;
+
+    /**
+     * Constructor de la simulación.
+     *
+     * @param mesActual mes inicial en el que comienza la simulación.
+     */
+    public Simulacion(int mesActual){
+        this.mesActual = mesActual;
+    }
+
+    /**
+     * Inicializa la lista de usuarios que participarán en la simulación.
+     * debe agregar a cada uno de los usuarios a la lista, con su respectivo dinero incial
+     */
+    public void inicializarUsuarios(){
+        usuarios = new ArrayList<>();
+        usuarios.add(new Usuario("Alicia", 15000.00));
+        usuarios.add(new Usuario("Bob", 2400.00));
+        usuarios.add(new Usuario("Cesar", 5000.00));
+        usuarios.add(new Usuario("diego", 9000.00));
+        usuarios.add(new Usuario("Erika", 10000.00));
+        usuarios.add(new Usuario("Fausto", 5000.00));
+    }
+
+    /**
+     * Inicializa la lista de servicios que estarán disponibles en la simulación.
+     */
+    public void inicialzarServicios(){
+        servicios = new ArrayList<>();
+        //se debe crear una instancia de cada servico y agregarla a servicios
+        Servicio HVOMax = new HVOMax("HVO Max");
+        servicios.add(HVOMax);
+
+        Servicio Memeflix = new Memeflix("Memeflix");
+        servicios.add(Memeflix);
+
+        Servicio MomazonPrime = new MomazonPrime("MomazonPrime");
+        servicios.add(MomazonPrime);
+        
+        Servicio Spootify = new Spootify("Spootify");
+        servicios.add(Spootify);
+
+        Servicio Thisney = new servicios.Thisney("Thisney");
+        servicios.add(Thisney);
+    }
+
+    /**
+     * Ejecuta el flujo completo de la simulación.
+     */
+    public void ejecutarSimulacioh(){
+
+    }
+
+    /**
+     * Procesa todas las operaciones correspondientes a un mes de la simulación.
+     */
+    public void procesarMes(){
+
+    }
+
+    /**
+     * Aplica el comportamiento definido para el usuario Alicia en un mes específico.
+     *
+     * @param mes mes en el que se ejecuta el comportamiento.
+     */
+    public void aplicarComportamientoAlicia(int mes){
+
+    }
+
+    /**
+     * Aplica el comportamiento definido para el usuario Bob en un mes específico.
+     *
+     * @param mes mes en el que se ejecuta el comportamiento.
+     */
+    public void aplicarComportamientoBob(int mes){
+
+    }
+
+    /**
+     * Aplica el comportamiento definido para el usuario Cesar en un mes específico.
+     *
+     * @param mes mes en el que se ejecuta el comportamiento.
+     */
+    public void aplicarComportamientoCesar(int mes){
+
+    }
+
+    /**
+     * Aplica el comportamiento definido para el usuario Diego en un mes específico.
+     *
+     * @param mes mes en el que se ejecuta el comportamiento.
+     */
+    public void aplicarComportamientoDiego(int mes){
+
+    }
+
+    /**
+     * Aplica el comportamiento definido para el usuario Erika en un mes específico.
+     *
+     * @param mes mes en el que se ejecuta el comportamiento.
+     */
+    public void aplicarComportamientoErika(int mes){
+
+    }
+
+    /**
+     * Aplica el comportamiento definido para el usuario Fausto en un mes específico.
+     *
+     * @param mes mes en el que se ejecuta el comportamiento.
+     */
+    public void aplicarComportamientoFausto(int mes){
+
+    }
+
+    /**
+     * Suscribe a un usuario a un servicio bajo una estrategia de cobro específica.
+     *
+     * @param usuario usuario que se suscribe.
+     * @param servicio servicio al que se suscribe el usuario.
+     * @param estrategia estrategia de cobro utilizada.
+     * @return {@code true} si la suscripción fue exitosa, 
+     *          {@code false} en caso contrario.
+     */
+    public boolean suscribirUsuarioAServicio(Usuario usuario, Servicio servicio, EstrategiaCobro estrategia){
+        return false;
+    }
+
+    /**
+     * Cancela la suscripción de un usuario a un servicio.
+     *
+     * @param usuario usuario cuya suscripción será cancelada.
+     * @param servicio servicio al cual estaba suscrito el usuario.
+     * @return {@code true} si la cancelación fue exitosa, 
+     *          {@code false} en caso contrario.
+     */
+    public boolean cancelarSUscripcion(Usuario usuario, Servicio servicio){
+        return false;
+    }
+
+    /**
+     * Cambia la estrategia de cobro de la suscripción de un usuario a un servicio.
+     *
+     * @param usuario usuario cuya suscripción será modificada.
+     * @param servicio servicio al que está suscrito el usuario.
+     * @param estrategia nueva estrategia de cobro a aplicar.
+     * @return {@code true} si el cambio fue exitoso,
+     *           {@code false} en caso contrario.
+     */
+    public boolean cambiarSuscripcionUsuario(Usuario usuario, Servicio servicio, EstrategiaCobro estrategia){
+        return false;
+    }
+
+    /**
+     * Genera un reporte con todas las operaciones registradas en la simulación.
+     */
+    public void generarReporteCompleto(){
+
+    }
+
+    /**
+     * Busca y devuelve un usuario a partir de su nombre.
+     *
+     * @param nombre nombre del usuario a buscar.
+     * @return el objeto {@link Usuario} correspondiente, o 
+     *              {@code null} si no existe.
+     */
+    public Usuario obtenerUsuario(String nombre){
+        //aqui va su codigo
+        return null;
+    }
+
+    /**
+     * Busca y devuelve un servicio a partir de su nombre.
+     *
+     * @param nombre nombre del servicio a buscar.
+     * @return el objeto {@link Servicio} correspondiente, o 
+     *              {@code null} si no existe.
+     */
+    public Servicio obtenerServicio(String nombre){
+        // aqui va su codigo 
+        return null;
+    }
 }
