2d3dcb94406d78c8770b95b53f405eaabd8c0e87
SaulVNavas <saulvnavas@ciencias.unam.mx>
2025-09-21 21:40:14 -0600

    hotfix: Comentario basura de merge en CampoFactory borrado, y valores en CampoConcreteFactory cambiados con respecto al correo de Gael.

--- Practica03_Equipo-Polimorfo/src/main/Academia/Campos/CampoFactory.java
+++ Practica03_Equipo-Polimorfo/src/main/Academia/Campos/CampoFactory.java
@@ -5,7 +5,6 @@ package main.Academia.Campos;
  * Interfaz del patron Factory para la creacion de campos de entrenamiento.
  * Define el contrato para las fabricas que crean diferentes tipos de Campo entranimeinto.
  * 
->>>>>>> 6377a9a (Mis cambios locales antes de pull)
  * @author Equipo-Polimorfo
  * @version 1.0
  */
@@ -14,7 +13,6 @@ public interface CampoFactory {
     /** 
      * Crea un campo de entranimiento dependiendo la suma de niveles de los integrantes.
      * @param sumaNiveles La suma total de los integrantes.
->>>>>>> 6377a9a (Mis cambios locales antes de pull)
      */
     CampoEntrenamiento crearCampo(int sumaNiveles);
 }
