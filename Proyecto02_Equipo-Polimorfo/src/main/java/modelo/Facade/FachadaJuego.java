c11b4b1d2739ba863b9f9e606fc697e12c90815f
Soto Mendoza Ismael de Jesus <isma132006@ciencias.unam.mx>
2025-11-03 09:54:56 -0600

    Solo los pies

---
+++ Proyecto02_Equipo-Polimorfo/src/main/java/modelo/Facade/FachadaJuego.java
@@ -0,0 +1,110 @@
+package main.java.modelo.Facade;
+
+import java.util.List;
+import main.java.modelo.state.estados.EstadoJuego;
+
+/**
+ * La clase {@code FachadaJuego} implementa el patrón de diseño Facade,
+ * proporcionando una interfaz simplificada para la gestión de los distintos
+ * subsistemas del videojuego (controlador, puntajes, autenticación, IA, niveles, etc.).
+ * <p>
+ * Esta clase actúa como punto de acceso central para operaciones comunes del juego,
+ * delegando las tareas específicas a las clases especializadas correspondientes.
+ * </p>
+ *
+ * @author Equipo-polimorfo
+ * @version 1.0
+ */
+public class FachadaJuego {
+
+    private ControladorJuego controladorJuego;
+    private GestorPuntajes gestorPuntajes;
+    private ServicioAutenticacion servicioAutenticacion;
+    private ServicioIA servicioIA;
+    private FabricaNiveles fabricaNiveles;
+
+    /**
+     * Inicia un nuevo juego según el modo especificado.
+     * 
+     * //CHECAR SI ES CLASICOS O JUGAR CONTRA IA O 2VS2
+     * @param modo el modo de juego seleccionado (clasico, breakut)
+     */
+    public void iniciarNuevoJuego(ModoJuego modo) {
+        //aqui va su codigo
+    }
+
+    /**
+     * Pausa el juego actual, preservando su estado.
+     * Este método puede detener los temporizadores, música y actualizaciones lógicas.
+     */
+    public void pausarJuego() {
+        //aqui va su codigo
+    }
+
+    /**
+     * Reanuda el juego desde un estado previamente pausado.
+     * Restaura las actualizaciones lógicas y de audio (si es que lleva) detenidas.
+     */
+    public void reanudarJuego() {
+        //aqui va su codigo
+    }
+
+    /**
+     * Finaliza el juego actual, liberando los recursos utilizados.
+     * Registra los puntajes obtenidos y muestra los detalles..
+     */
+    public void terminarJuego() {
+        //aqui va su codigo
+    }
+
+    /**
+     * Guarda el progreso actual del juego..
+     */
+    public void guardarJuego() {
+        //aqui va su codigo
+    }
+
+    /**
+     * Carga un estado de juego previamente guardado.
+     *
+     * @return el {@link EstadoJuego} restaurado desde donde se guarda
+     */
+    public EstadoJuego cargarJuego() {
+        //aqui va su codigo
+        return null;
+    }
+
+    /**
+     * Obtiene la lista de puntajes más altos registrados.
+     *
+     * @return una lista de objetos {@code Puntaje} que representan los puntajes más altos
+     */
+    public List<Puntaje> obtenerPuntajesAltos() {
+        //aqui va su codigo
+        return null;
+    }
+
+    /**
+     * Crea un nuevo nivel en función de la configuración proporcionada por el user.
+     *
+     * @param config la configuración del nivel a crear
+     * @return el nuevo objeto {@code Nivel} generado
+     */
+    public Nivel crearNivel(ConfigNivel config) {
+        //aqui va su codigo
+        return null;
+    }
+
+    /**
+     * Autentica a un usuario en el sistema mediante sus datos del registro
+     *
+     * @param usuario el nombre de usuario
+     * @param clave la contraseña del usuario
+     * @return el objeto {@code Usuario} autenticado si los datoss son válidos,
+     *         o {@code null} en caso contrario
+     */
+    public Usuario autenticarUsuario(String usuario, String clave) {
+        //aqui va su codigo
+        return null;
+    }
+}
---
+++ Proyecto02_Equipo-Polimorfo/src/main/java/modelo/observer/ObservadorUI.java
@@ -0,0 +1,26 @@
+package main.java.modelo.observer;
+
+public class ObservadorUI implements ObservadorJuego {
+    
+    private VistaJuego vistaJuego;
+
+    @Override
+    public void alcambiarPuntaje(int jugador, int nuevoPuntaje) {
+        // aqui va su codigo 
+    }
+
+    @Override
+    public void alTerminarJuego(int ganador) {
+        // aqui va su codigo 
+    }
+
+    @Override
+    public void alCompletarNivel() {
+        // aqui va su codigo 
+    }
+
+    @Override
+    public void alGenrarItem(Item item) {
+        // aqui va su codigo 
+    }
+}
